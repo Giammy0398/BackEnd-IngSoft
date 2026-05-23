ab6e35c577cfc75e515782935096369cca914ecb
Gianmarco Trapella <138238432+Giammy0398@users.noreply.github.com>
2026-05-23 12:20:38 +0200

    Add files via upload

---
+++ BackEnd-Main-IngSoft/src/main/java/com/example/backendmainingsoft/BackEndMainIngSoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendmainingsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndMainIngSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndMainIngSoftApplication.class, args);
+    }
+
+}
